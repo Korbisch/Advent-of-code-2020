e814ef845d05fd24d03a7c9838e583be61ed6189
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2021-04-26 16:50:25 +0200

    day 8 part 2

--- day8.py
+++ day8.py
@@ -11,10 +11,7 @@ acc = 0
 index = 0
 already_visited = set()
 
-while index < len(operations):
-    if index in already_visited:
-        print('result:', acc)
-        break
+while index not in already_visited:
     already_visited.add(index)
 
     operation = operations[index][:3] # acc, jmp or nop
@@ -34,3 +31,59 @@ while index < len(operations):
             index += value
         else:
             index -= value
+
+print('accumulator:', acc)
+
+# part 2
+
+# keeps track of the previous index -> is the operation that should be changed
+acc = 0
+index = 0
+already_visited = set()
+changed_operations = set()
+op_count = 0
+
+while index < len(operations):
+
+    operation = operations[index][:3] # acc, jmp or nop
+    operator = operations[index][4:5] # + or -
+    value = int(operations[index][5:-1]) # value eg. 50
+
+    # try the change
+    if op_count == 0 and index not in changed_operations and (operation in ['jmp','nop']):
+        # change operation
+        if operation == 'jmp':
+            operation = 'nop'
+        elif operation == 'nop':
+            operation = 'jmp'
+        # add the index you have tried to change
+        changed_operations.add(index)
+        op_count = 1
+
+    if index in already_visited:
+        # loop -> reset and restart
+        index = 0
+        acc = 0
+        operation = ''
+        already_visited.clear()
+        op_count = 0
+
+    # don't add on reset
+    if operation != '':
+        already_visited.add(index)
+
+    if operation == 'nop':
+        index += 1
+    if operation == 'acc':
+        if operator == '+':
+            acc += value
+        else:
+            acc -= value
+        index += 1
+    if operation == 'jmp':
+        if operator == '+':
+            index += value
+        else:
+            index -= value
+
+print('accumulator correct execution:', acc)
