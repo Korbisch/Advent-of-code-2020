4d80aa00e23823f296a33d24c08768d852742b56
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2021-04-27 10:56:19 +0200

    refactor

--- day8.py
+++ day8.py
@@ -1,89 +1,107 @@
 # advent of code 2020 day 8 part 1 & 2
 
-# list of operations
-operations = []
+import os
+# path to file
+dir = os.path.dirname(__file__)
+file = os.path.join(dir, 'inputs/day8.txt')
 
-# read in file line by line
-with open('/Users/korbinianschleifer/Developer/advent_of_code_2020/inputs/day8.txt', 'r') as file:
-    operations = file.readlines()
+# list of lines in the program
+program = list()
 
-acc = 0
-index = 0
-already_visited = set()
+# read file line by line
+with open(file, 'r') as file:
+    for i,line in enumerate(file):
+        program.insert(i, dict())
+        program[i]['operation'] = line[:3]
+        program[i]['operator'] = line[4:5]
+        program[i]['value'] = int(line[5:-1])
 
-while index not in already_visited:
-    already_visited.add(index)
+# part 1
+def calc_acc_sum_on_fail(program: list) -> int:
+    acc_sum = 0
+    index = 0
+    already_visited = set()
 
-    operation = operations[index][:3] # acc, jmp or nop
-    operator = operations[index][4:5] # + or -
-    value = int(operations[index][5:-1]) # value eg. 50
+    while index not in already_visited:
+        already_visited.add(index)
 
-    if operation == 'nop':
-        index += 1
+        operation = program[index]['operation'] # acc, jmp or nop
+        operator = program[index]['operator'] # + or -
+        value = program[index]['value'] # value eg. 50
+
+        # change index
+        index += add_new_index(operation, operator, value)
+        
+        # add to accumulator
+        acc_sum += add_to_acc(operation, operator, value)
+    
+    return acc_sum
+
+# calculates the value that should be added to the index
+def add_new_index(operation, operator, value) -> int:
+    if operation in ['nop', 'acc']:
+        return 1
+    elif operation == 'jmp':
+        return value if operator == '+' else -value
+    return 0
+
+# calculates the value that should be added to the accumulator
+def add_to_acc(operation, operator, value) -> int:
     if operation == 'acc':
-        if operator == '+':
-            acc += value
-        else:
-            acc -= value
-        index += 1
-    if operation == 'jmp':
-        if operator == '+':
-            index += value
-        else:
-            index -= value
+        return value if operator == '+' else -value
+    return 0
 
-print('accumulator:', acc)
+print('accumulator sum on failure:', calc_acc_sum_on_fail(program))
 
 # part 2
 
-# keeps track of the previous index -> is the operation that should be changed
-acc = 0
-index = 0
-already_visited = set()
-changed_operations = set()
-op_count = 0
+# calculate the sum on correct execution
+def calc_acc_sum_on_exec(program: list) -> int:
+    acc = 0
+    index = 0
+    already_visited = set()
+    changed_operations = set()
+    op_count = 0
 
-while index < len(operations):
+    while index < len(program):
 
-    operation = operations[index][:3] # acc, jmp or nop
-    operator = operations[index][4:5] # + or -
-    value = int(operations[index][5:-1]) # value eg. 50
+        operation = program[index]['operation'] # acc, jmp or nop
+        operator = program[index]['operator'] # + or -
+        value = program[index]['value'] # value eg. 50
 
-    # try the change
-    if op_count == 0 and index not in changed_operations and (operation in ['jmp','nop']):
-        # change operation
-        if operation == 'jmp':
-            operation = 'nop'
-        elif operation == 'nop':
-            operation = 'jmp'
-        # add the index you have tried to change
-        changed_operations.add(index)
-        op_count = 1
+        # try the change
+        if op_count == 0 and index not in changed_operations and (operation in ['jmp','nop']):
+            # change the operation
+            operation = switch_operations(operation)
+            # add the index you have tried to change
+            changed_operations.add(index)
+            op_count = 1
 
-    if index in already_visited:
-        # loop -> reset and restart
-        index = 0
-        acc = 0
-        operation = ''
-        already_visited.clear()
-        op_count = 0
+        if index in already_visited:
+            # loop -> reset and restart
+            index = 0
+            acc = 0
+            operation = ''
+            already_visited.clear()
+            op_count = 0
+            continue
 
-    # don't add on reset
-    if operation != '':
         already_visited.add(index)
 
-    if operation == 'nop':
-        index += 1
-    if operation == 'acc':
-        if operator == '+':
-            acc += value
-        else:
-            acc -= value
-        index += 1
-    if operation == 'jmp':
-        if operator == '+':
-            index += value
-        else:
-            index -= value
+        # change index
+        index += add_new_index(operation, operator, value)
 
-print('accumulator correct execution:', acc)
+        # add to accumulator
+        acc += add_to_acc(operation, operator, value)
+
+    return acc
+
+# changes the operation from jmp to nop and from nop to jmp
+def switch_operations(operation: str) -> str:
+    if operation == 'jmp':
+        return 'nop'
+    elif operation == 'nop':
+        return 'jmp'
+    return operation
+
+print('accumulator correct execution:', calc_acc_sum_on_exec(program))
