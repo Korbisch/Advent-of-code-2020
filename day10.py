23d25428b1f534db94756c4d36f08441dccbb9f7
Korbinian Schleifer <korbinian.schleifer@gmail.com>
2021-05-01 13:28:20 +0200

    day 2 part 2

--- day10.py
+++ day10.py
@@ -33,6 +33,7 @@ def find_solution(numbers: list) -> int:
 
     # built in adapter is 3 higher than last element
     three_jolt_dif += 1
+    numbers.remove(0)
 
     return one_jolt_dif * three_jolt_dif
 
@@ -40,3 +41,24 @@ print('solution 1:', find_solution(numbers))
 
 
 ### Part 2 ###
+
+def find_solution2(numbers: list) -> int:
+    solution = {0:1} # keeps track of numbers and current possibilities
+    numbers.sort()
+    for number in numbers:
+        # initialize dictionary for this number
+        solution[number] = 0
+        # possibilities are (pos. num - 1) + (pos. num - 2) + (pos. num - 3)
+        if number - 1 in solution:
+            solution[number] += solution[number-1]
+        if number - 2 in solution:
+            solution[number] += solution[number-2]
+        if number - 3 in solution:
+            solution[number] += solution[number-3]
+
+    return solution[numbers[-1]]
+
+print('test should be 8:', find_solution2([1,4,5,6,7,10,11,12,15,16,19]))
+test = [28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3]
+print('test should be 19208:', find_solution2(test))
+print('solution 2:', find_solution2(numbers))
